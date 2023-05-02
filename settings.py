c64ef2d95a4ebef7d6c064c68389538e25bb9349
niikpakpobrown <brown.niikpakpo@gmail.com>
2023-05-02 23:07:43 +0000

    Class to handle the settings for the game

---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    """A class to store all the setting for the game"""
+
+    def __init__(self):
+        """Initialize the settings"""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+        
